e5b4fbe6ebedcb4e287905df12e0e6fafe8bd1eb
Mandi Grant <mandi018@gmail.com>
2014-08-10 12:49:06 -0700

    Now generates a package.json file and prompts user to fill it out as part of the Yeoman script

--- generator-mjgrant/app/index.js
+++ generator-mjgrant/app/index.js
@@ -1,10 +1,114 @@
 var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.generators.Base.extend({
-    method1: function() {
-      console.log('method 1 just ran');
+  promptUser: function() {
+    var done = this.async();
+    console.log(this.yeoman);
+
+    var prompts = [
+      {
+      name: 'siteTitle',
+      message: 'Site or app title:'
+    },{
+      name: 'authorName',
+      message: 'Site author:'
+    }, {
+      name: 'appName',
+      message: 'node app name',
+      default: 'none'
+    }, {
+      name: 'description',
+      message: 'Description',
+      default: 'none'
+    }, {
+      name: 'version',
+      message: 'Version',
+      default: '0.0.1'
+    }, {
+      name: 'repository',
+      message: 'Project git repository'
+    }, {
+      name: 'homepage',
+      message: 'Project homepage'
+    }, {
+      name: 'license',
+      message: 'License',
+      default: 'MIT'
+    }, {
+      name: 'authorName',
+      message: 'Author name'
+    }, {
+      name: 'authorEmail',
+      message: 'Author email'
+    }, {
+      name: 'authorUrl',
+      message: 'Author url'
+    }, {
+      name: 'nodeVersion',
+      message: 'What versions of node does it run on?',
+      default: '>= 0.8.0'
     },
-    method2: function() {
-      console.log('method 2 just ran');
-    }
+  ];
+  
+    this.prompt(prompts, function(props) {
+      this.siteTitle = props.siteTitle;
+      this.authorName = props.authorName;
+      this.appName = props.appName;
+      this.description = props.description;
+      this.shortName = props.name;
+
+      if (!props.homepage) {
+        props.homepage = this.repoUrl;
+      }
+
+      this.props = props;
+
+      done();
+    }.bind(this));
+  },
+
+  scaffoldFolders: function() {
+    this.mkdir("js");
+    this.mkdir("stylesheets");
+  },
+
+  copyMainFiles: function() {
+    this.copy("_scripts.js","js/scripts.js");
+    this.copy("_style.css", "stylesheets/style.css");
+    this.copy("_gitignore", ".gitignore");
+    this.copy("_server.js","server.js");
+
+    var context = {
+      site_name: this.siteTitle,
+      author_name: this.authorName,
+      site_description: this.siteDescription,
+    };
+
+    this.template("_index.html", "index.html", context);
+    this.template("_package.json", "package.json", context);
+  },
+
+  createPkg: function() {
+    var pkgFile = {
+      name: this.appName,
+      version: this.props.version,
+      description: this.props.description,
+      homepage: this.props.homepage,
+      repository: this.props.repository,
+      author: {
+        name: this.props.authorOriginalName,
+        email: this.props.authorEmail,
+        url: this.props.authorUrl
+      },
+      keywords: [],
+      main: 'index.js',
+      engines: {
+        node: this.props.nodeVersion
+      },
+      license: this.props.license,
+      devDependencies: {},
+      scripts: {}
+    };
+  this.writeFileFromString(JSON.stringify(pkgFile, null, 2), 'package.json');
+  },
 });
